4ea2c3b28c344c3a8d7cd327c2a85b871b98e93e
Bartosz <80819432+Bartosz278@users.noreply.github.com>
2023-09-15 15:07:44 +0200

    Add files via upload

---
+++ dist/app.js
@@ -0,0 +1,37 @@
+const object = document.getElementById('ball');
+let valueX = Math.floor(Math.random() * -260) - 10;
+let valueY = Math.floor(Math.random() * -260) - 10;
+object.style.bottom = `${valueY}px`;
+object.style.right = `${valueX}px`;
+document.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 65:
+            if (valueX > -10) {
+                break;
+            }
+            valueX = valueX + 10;
+            object.style.right = `${valueX}px`;
+            break;
+        case 87:
+            if (valueY > -10) {
+                break;
+            }
+            valueY = valueY + 10;
+            object.style.bottom = `${valueY}px`;
+            break;
+        case 68:
+            if (valueX < -270) {
+                break;
+            }
+            valueX = valueX - 10;
+            object.style.right = `${valueX}px`;
+            break;
+        case 83:
+            if (valueY < -270) {
+                break;
+            }
+            valueY = valueY - 10;
+            object.style.bottom = `${valueY}px`;
+            break;
+    }
+};
